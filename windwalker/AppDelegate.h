3d868dc870e2138611edaa67ebaa2a9983f23c21
Jason C <jforeza@gmail.com>
2018-06-22 10:22:15 -0700

    Initial project commit after adding PodFile + linking in AdMob framework

---
+++ windwalker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  windwalker
+//
+//  Created by Jason C on 6/22/18.
+//  Copyright © 2018 Jason C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
